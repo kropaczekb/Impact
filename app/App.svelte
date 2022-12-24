2e1e7f88b855c670613b470467b35630b7abe286
Brian Kropaczek <kropaczekb@gmail.com>
2022-12-24 16:32:34 -0500

    Pie graph works

--- app/App.svelte
+++ app/App.svelte
@@ -25,9 +25,4 @@
     display: flex;
     background-color: grey;
   }
-  img {
-    width: 20px;
-    height: 20px;
-    border-radius: 50%;
-  }
 </style>
--- app/routes/DriveLess.svelte
+++ app/routes/DriveLess.svelte
@@ -1,5 +1,6 @@
 <script>
   import * as d3 from "d3"
+  import { afterUpdate, onMount } from "svelte";
 
   let milesPerWeek = 1
   let reducedMiles = 1
@@ -9,6 +10,83 @@
   $: coReduction = 0.000445334 * reducedMiles
   $: percentCoReduction = coReduction * 52 / totalEmissions * 100
   $: everyCoReduction = coReduction * 135000000 * 52 / totalEmissions * 100
+  $: pieData = [ newTarget, reducedMiles ]
+  let testData = [ 1, 2, 3, 4, 5 ]
+  var data = [2, 4, 8, 10];
+
+  afterUpdate(() => {
+    var svg = d3.select("svg"),
+        width = svg.attr("width"),
+        height = svg.attr("height"),
+        radius = Math.min(width, height) / 2,
+        g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    var color = d3.scaleOrdinal(['#4daf4a','#377eb8','#ff7f00','#984ea3','#e41a1c']);
+
+    // Generate the pie
+    var pie = d3.pie();
+
+    // Generate the arcs
+    var arc = d3.arc()
+                .innerRadius(0)
+                .outerRadius(radius);
+
+    //Generate groups
+    var arcs = g.selectAll("arc")
+                .data(pie(pieData))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+
+    //Draw arc paths
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+            return color(i);
+        })
+        .attr("d", arc);
+
+  })
+  // let width = 300
+  // let height = 200
+  // let svg = d3.select("#pie"),
+  //   // width = svg.attr("width"),
+  //   // height = svg.attr("height"),
+  //   radius = Math.min(width, height) / 2,
+  //   g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  // let color = d3.scaleOrdinal(["#98abc5", "#8a89a6"])
+  // let pie = d3.pie()
+  // let arc = d3.arc()
+  //   .innerRadius(0)
+  //   .outerRadius(radius)
+
+  // let arcs = g.selectAll("arc")
+  //   .data(pie(testData))
+  //   .enter()
+  //   .append("g")
+  //   .attr("class", "arc")
+
+  // arcs.append("path")
+  //   .attr("fill", function(d, i) {
+  //     return color(i)
+  //   })
+  //   .attr("d", arc)
+
+
+  //////////////////////
+    // .value(function(d) {return d.value})
+  // let data_ready = pie(Object.entries(pieData))
+
+  // svg.selectAll('whatever')
+  //   .data_ready(data_ready)
+  //   .append('path')
+  //   .attr('d', d3.arc()
+  //     .innerRadius(0)
+  //     .outerRadius(radius)
+  //   )
+  //   .attr('fill', function(d) { return(color(d.pieData.key))})
+  //   .attr("stroke", "black")
+  //   .style("stroke-width", "2px")
+  //   .style("opacity", 0.7)
 </script>
 
 <input type=number bind:value={milesPerWeek} min={0}>
@@ -17,4 +95,4 @@
 <p>How much CO2 that's saving: {coReduction} tons</p>
 <p>How much that affects our total: {percentCoReduction}%</p>
 <p>If everyone in the U.S. also drove that many miles less: {everyCoReduction}%</p>
-<p></p>
+<svg id="pie" width="300" height="200"></svg>
